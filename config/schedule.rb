e1f87509e9a96e71fcb504f88f6724ae56d8887c
Spencer Elliott <me@elliottsj.com>
2014-09-07 13:18:22 -0400

    Add rake task to scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,8 +3,7 @@ every 1.day, at: '4:00 am' do
   rake 'backup'
 end
 
-# Recompile the site to 
+# Recompile the site to refresh visible posts
 every 1.day, at: '12:00 am' do
-  # TODO: recompile the site
-  command 'pwd'
+  rake 'deploy'
 end
